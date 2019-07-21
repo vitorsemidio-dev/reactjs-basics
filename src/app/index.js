107ce50f8b7b844b06bf1bc6d07c39114668faf3
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-07-21 16:07:23 -0300

    First Component & CDN Bootstrap

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,18 @@
-console.log('it works');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Hello Academind</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+render(<App/>, document.getElementById('app'))
