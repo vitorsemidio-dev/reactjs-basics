3fd99c5e8abba4f4c4feee960132972b6771afed
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-07-22 00:22:06 -0300

    Events & State of Components

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-export class Home extends Component {
+
+
+export default class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            age: 0,
+            name: '',
+        }
+    };
+
+    componentWillMount() {
+        const { age } = this.props;
+        this.setState({ age })
+    }
+
+    onMakeOlder() {
+        let { age } = this.state;
+        age++;
+        this.setState({ age });
+    }
+
+    onMakeNewer() {
+        let { age } = this.state;
+        age--;
+        this.setState({ age });
+    }
     render() {
-        const { name, age, user, children } = this.props;
+        const { name } = this.props;
         return (
             <div>
                 <p>In a new Component</p>
-                <p>Your name is {name}, your age is {age}</p>
-                <p>User object => Name: {user.name}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        {user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-                    </ul>
-                    {children}
+                <p>Your name is {name}, your age is {this.state.age} </p>
+                <hr/>
+                <div className="buttons">
+                    <button 
+                        className="btn btn-outline-primary"
+                        onClick={() => this.onMakeNewer()}>
+                        Make me newer
+                    </button>
+                    <button
+                        className="btn btn-outline-primary"
+                        onClick={this.onMakeOlder.bind(this)}>
+                        Make me older
+                    </button>                    
                 </div>
             </div>
         )
@@ -23,6 +54,4 @@ export class Home extends Component {
 Home.propTypes = {
     name: PropTypes.string,
     age: PropTypes.number,
-    user: PropTypes.object,
-    children: PropTypes.element.isRequired,
 };
--- src/app/index.js
+++ src/app/index.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import { Header } from './components/Header';
-import { Home } from './components/Home';
+import Home from './components/Home';
 
 class App extends Component {
     render() {
-        let user = {
-            name: 'Emidio',
-            hobbies: ['Sports', 'Games', 'Study']
-        }
         return (
             <div className="container">
                 <div className="row">
@@ -19,9 +15,7 @@ class App extends Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={'Eren'} age={20} user={user}>
-                            <p>Filho no corpo da tag Home</p>
-                        </Home>
+                        <Home name={'Eren'} age={20}/>
                     </div>
                 </div>
             </div>
